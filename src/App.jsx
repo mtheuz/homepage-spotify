70b639c147fd427565c60aa59f5d2912beafcd58
Matheus Mota <matheuzwork@gmail.com>
2023-12-14 15:20:08 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+export default function App() {
+  return (
+    <div className="p-2 bg-slate-400">
+      <h1 className= "rounded-md text-3xl font-bold underline">
+      Hello world!
+      </h1>
+    </div>
+    
+  )
+}
